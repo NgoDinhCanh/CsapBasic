a89a8451c260a62ee0d58229b3d6b46840dd2c74
NgoDinhCanh <77870743+NgoDinhCanh@users.noreply.github.com>
2021-11-15 20:41:51 +0700

    TernaryOperater

---
+++ Controls/TernaryOperater.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controls
+{
+    class TernaryOperater
+    {
+        static void Main()
+        {
+            // Toán tử 3 ngôi 
+            // variable = (condition)? expressionTrue : expressionFalse ;
+            int time = 12;
+            string msg = default;
+            //if (time < 12)
+            //{
+            //    msg = "Hello";
+            //}
+            //else
+            //{
+            //    msg = "Goodbye";
+            //}
+            // 
+            msg = (time < 12) ? "Hello" : "Goodbye";
+
+            Console.WriteLine(msg);
+
+
+
+        }
+    }
+}
