748b3da3924a8dfe5ece3cfcaab8e631c174abdc
NgoDinhCanh <77870743+NgoDinhCanh@users.noreply.github.com>
2021-11-15 20:32:27 +0700

    SwitchCase

--- Controls/IfElse.cs
+++ Controls/IfElse.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Controls
 {
-    class Program
+    class IfElse
     {
         static void Main(string[] args)
         {
---
+++ Controls/SwitchCase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controls
+{
+    class SwitchCase
+    {
+        static void Main()
+        {
+            /*
+             * switch (expression)
+             * case x:
+             *  code block
+             *   break;
+             * case y :
+             *      code block
+             *  break;
+             * default
+             *      code block
+             *  break;
+             */
+            int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+            switch (dayOfWeek)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Nhập vào đúng ngày trong tuần (1-7)");
+                    break;
+
+            }
+        }
+    }
+}
