9eab7279c6795101ff6e0467fbdf95db62483e63
NgoDinhCanh <77870743+NgoDinhCanh@users.noreply.github.com>
2021-11-16 14:54:27 +0700

    DoWhile

---
+++ LearnLoopControls/DoWhile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnLoopControls
+{
+    class DoWhile
+    {
+        static void Main()
+        {
+            int x = 90;
+            // Vong lap nay luon thuc hien it nhat 1 lan 
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x < 100);
+        }
+    }
+}
