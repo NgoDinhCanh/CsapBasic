8db5506b7e462ce79817a113fc6419debb738f43
NgoDinhCanh <77870743+NgoDinhCanh@users.noreply.github.com>
2021-11-16 14:40:08 +0700

    LeanLoopControls

---
+++ LearnLoopControls/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnLoopControls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for loop 
+            // for (khoi tao ; dieu kien ; buoc nhay )
+            // {
+            //   Noi dung can lap 
+            // }
+            // Hien thi ra cac so le <= n 
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i <= n; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //{
+            Console.WriteLine("Nhap a <= b:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = a; i <= b; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"Sum for {a} to {b} = {sum}");
+
+
+        }
+    }
+}
