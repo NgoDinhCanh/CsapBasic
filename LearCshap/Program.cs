79e31ba84e232c833c947994c84f4ca0c7d285eb
NgoDinhCanh <77870743+NgoDinhCanh@users.noreply.github.com>
2021-11-18 14:50:19 +0700

    Mang

---
+++ LearCshap/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearCshap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * -Mảng là gì ? dùng làm cái gì ?
+             * -Khai báo , khởi tạo mảng
+             * -Sử dụng mảng
+             * -Duyệt mảng
+             * -Vòng lặp foreach
+             * -Sắp xếp mảng 
+             */
+            int a, b, c;
+            int[] number = new int[200];
+            string[] friends = new string[] {"Thanh","Thương","Khoa","Dat","Giang","Dien","Man"};
+            string[] cars = new string[100];
+            Console.WriteLine("Do dai mang:" + friends.Length);
+            Console.WriteLine(friends[3]);
+
+        }
+    }
+}
